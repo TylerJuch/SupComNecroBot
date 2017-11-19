3965457f7f5fe76b09b4b74cd8d79d9c0d725878
Tyler Juch <tyler.juch@gmail.com>
2017-11-18 23:32:12 -0600

    Indent code

--- necro.lua
+++ necro.lua
@@ -2,12 +2,10 @@ local oldPosition1=1
 local oldPosition2=1
 local oldPosition3=1
 
-RespawnUnit=function(blueprint,army,pos1,pos2,pos3)
-if oldPosition1~=pos1 or oldPosition3~=pos3 or oldPosition2~=pos2 then
-CreateUnitHPR(blueprint,army,pos1,pos2,pos3,0,0,0)
-oldPosition1=pos1
-oldPosition2=pos2
-oldPosition3=pos3
-end
-
+function RespawnUnit (blueprint, army, pos1, pos2, pos3)
+  if (oldPosition1 ~= pos1
+    or oldPosition2 ~= pos2
+    or oldPosition3 ~= pos3) then
+    CreateUnitHPR(blueprint, army, pos1, pos2, pos3, 0, 0, 0)
+  end
 end
