e5eb1aa9c283cb0bfc8d9463ed7ad915e148ab36
Tyler Juch <tyler.juch@gmail.com>
2017-11-19 01:07:10 -0600

    Still trying to not blow Prop up. Saving off old prop.

--- hook/lua/sim/Prop.lua
+++ hook/lua/sim/Prop.lua
@@ -1,23 +1,16 @@
-function Prop:GetReclaimCosts = function(self, reclaimer)
-  local reclaimTime = getReclaimTime(self, reclaimer)
+# Overwrite GetReclaimCosts so that the player does not get mass or energy when a necro is the reclaimer
+do
 
-  if EntityCategoryContains(categories.NECRO, reclaimer) then
-    return (reclaimTime/10), 1, 1
-  else
-    return (reclaimTime/10), self.EnergyReclaim, self.MassReclaim
-  end
-end
+local oldProp = Prop
+Prop = Class(oldProp) {
+  GetReclaimCosts = function(self, reclaimer)
+    local reclaimTime, energyReclaim, massReclaim = oldProp.GetReclaimCosts(self, reclaim)
 
-function Prop:getMassReclaimTime = function(self, reclaimerBlueprint)
-  return self.ReclaimTimeMassMult * (self.MassReclaim / reclaimer:GetBuildRate())
-end,
+    if EntityCategoryContains(categories.NECRO, reclaimer) then
+      return reclaimTime, 1, 1
+    else
+      return reclaimTime, energyReclaim, massReclaim
+    end
+}
 
-function Prop:getEnergyReclaimTime = function(self, reclaimerBlueprint)
-  return self.ReclaimTimeEnergyMult * (self.EnergyReclaim / reclaimer:GetBuildRate())
-end,
-
-function Prop:getReclaimTime = function (self, reclaimer)
-  local massReclaimTime = getMassReclaimTime(self, reclaimer)
-  local energyReclaimTime = getEnergyReclaimiTime(self, reclaimer)
-  return = math.max(massReclaimTime, energyReclaimTime)
 end
