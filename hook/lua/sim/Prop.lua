20b18e73f04ffbcc2b6c9b8c8782f9147b62cddd
Tyler Juch <tyler.juch@gmail.com>
2017-11-19 00:53:44 -0600

    Trying to not kill Prop

--- hook/lua/sim/Prop.lua
+++ hook/lua/sim/Prop.lua
@@ -1,26 +1,23 @@
-local oldProp = Prop
-Prop = Class(oldPropfile) {
-  GetReclaimCosts = function(self, reclaimer)
-    local reclaimTime = getReclaimTime(self, reclaimer)
+function Prop:GetReclaimCosts = function(self, reclaimer)
+  local reclaimTime = getReclaimTime(self, reclaimer)
 
-    if EntityCategoryContains(categories.NECRO, reclaimer) then
-      return (reclaimTime/10), 1, 1
-    else
-      return (reclaimTime/10), self.EnergyReclaim, self.MassReclaim
-    end
-  end,
-
-  getMassReclaimTime = function(self, reclaimerBlueprint)
-    return self.ReclaimTimeMassMult * (self.MassReclaim / reclaimer:GetBuildRate())
-  end,
-
-  getEnergyReclaimTime = function(self, reclaimerBlueprint)
-    return self.ReclaimTimeEnergyMult * (self.EnergyReclaim / reclaimer:GetBuildRate())
-  end,
-
-  getReclaimTime = function (self, reclaimer)
-    local massReclaimTime = getMassReclaimTime(self, reclaimer)
-    local energyReclaimTime = getEnergyReclaimiTime(self, reclaimer)
-    return = math.max(massReclaimTime, energyReclaimTime)
+  if EntityCategoryContains(categories.NECRO, reclaimer) then
+    return (reclaimTime/10), 1, 1
+  else
+    return (reclaimTime/10), self.EnergyReclaim, self.MassReclaim
   end
-}
+end
+
+function Prop:getMassReclaimTime = function(self, reclaimerBlueprint)
+  return self.ReclaimTimeMassMult * (self.MassReclaim / reclaimer:GetBuildRate())
+end,
+
+function Prop:getEnergyReclaimTime = function(self, reclaimerBlueprint)
+  return self.ReclaimTimeEnergyMult * (self.EnergyReclaim / reclaimer:GetBuildRate())
+end,
+
+function Prop:getReclaimTime = function (self, reclaimer)
+  local massReclaimTime = getMassReclaimTime(self, reclaimer)
+  local energyReclaimTime = getEnergyReclaimiTime(self, reclaimer)
+  return = math.max(massReclaimTime, energyReclaimTime)
+end
