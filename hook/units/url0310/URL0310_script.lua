ce887a84652ca321b93e47dfac970b7336ff03c7
Tyler Juch <tyler.juch@gmail.com>
2017-11-19 12:04:52 -0600

    Cybran unit updates

--- hook/units/url0310/URL0310_script.lua
+++ hook/units/url0310/URL0310_script.lua
@@ -3,22 +3,14 @@
 #**  File     :  /cdimage/units/URL0309/URL0309_script.lua
 #**  Author(s):  John Comes, David Tomandl
 #**
-#**  Summary  :  Cybran Tier 3 Engineer Script
+#**  Summary  :  Cybran Tier 3 Necro Script
 #**
-#**  Copyright © 2005 Gas Powered Games, Inc.  All rights reserved.
+#**  Copyright ï¿½ 2005 Gas Powered Games, Inc.  All rights reserved.
 #****************************************************************************
-
 local CConstructionUnit = import('/lua/cybranunits.lua').CConstructionUnit
-
-URL0310 = Class(CConstructionUnit) {
-    BuildBones = {
-        YawBone = 'Buildpoint_Center',
-        PitchBone = 'Buildpoint_Center',
-        BuildEffectBones = {'Buildpoint_Left','Buildpoint_Center','Buildpoint_Right'},
-    },
-    OnCreate=function(self)
-    self:SetCustomName("Necro")
+local URL0310 = Class(CConstructionUnit) {
+  OnCreate=function(self)
     CConstructionUnit.OnCreate(self)
-    end,
+  end
 }
 TypeClass = URL0310
