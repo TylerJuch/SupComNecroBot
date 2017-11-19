5db7b61c17d0315819df48f5ec87298c965bec46
Tyler Juch <tyler.juch@gmail.com>
2017-11-19 12:03:11 -0600

    UEF necro unit script updates

--- hook/units/ual0310/UAL0310_script.lua
+++ hook/units/ual0310/UAL0310_script.lua
@@ -3,15 +3,12 @@
 #**  File     :  /cdimage/units/UAL0309/UAL0310_script.lua
 #**  Author(s):  John Comes, David Tomandl
 #**
-#**  Summary  :  Aeon Unit Script
+#**  Summary  :  Aeon Tech 3 Necro
 #**
 #**  Copyright � 2005 Gas Powered Games, Inc.  All rights reserved.
 #****************************************************************************
-#
-# AEON TECH 3 NECRO
-#
 local AConstructionUnit = import('/lua/aeonunits.lua').AConstructionUnit
-UAL0310 = Class(AConstructionUnit) {
+local UAL0310 = Class(AConstructionUnit) {
   OnCreate = function(self)
     AConstructionUnit.OnCreate(self)
   end
--- hook/units/uel0310/UEL0310_script.lua
+++ hook/units/uel0310/UEL0310_script.lua
@@ -3,24 +3,14 @@
 #**  File     :  /cdimage/units/UEL0309/UEL0309_script.lua
 #**  Author(s):  John Comes, David Tomandl
 #**
-#**  Summary  :  Terran Tech 3 Engineer
+#**  Summary  :  UEF Tech 3 Necro
 #**
-#**  Copyright © 2005 Gas Powered Games, Inc.  All rights reserved.
+#**  Copyright ï¿½ 2005 Gas Powered Games, Inc.  All rights reserved.
 #****************************************************************************
 local TConstructionUnit = import('/lua/terranunits.lua').TConstructionUnit
-
 local UEL0310 = Class(TConstructionUnit) {
-    BuildBones = {
-        AimBone = 0,
-        PitchBone = 'Turret_Barrel',
-        YawBone = 'Turret',
-        BuildEffectBones = { 'Turret_Muzzle01', 'Turret_Muzzle02', 'Turret_Muzzle03' },
-    },
-    
-    OnCreate=function(self)
-    self:SetCustomName("Necro")
+  OnCreate = function(self)
     TConstructionUnit.OnCreate(self)
-    end,
+  end
 }
-
 TypeClass = UEL0310
