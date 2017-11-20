c11c84235bda5ad01de8e8106b7a6b515f2a3ccc
Tyler Juch <tyler.juch@gmail.com>
2017-11-19 20:07:13 -0600

    Fixed bug with multiple necros resurrecting the same unit multiple times

--- hook/lua/sim/Unit.lua
+++ hook/lua/sim/Unit.lua
@@ -14,12 +14,18 @@ Unit = Class(oldUnit) {
   OnStopReclaim = function(self, target)
     oldUnit.OnStopReclaim(self, target)
 
+    LOG("OnStopReclaim Target:")
+    LOG(repr(target))
+
     # If the reclaimer is a necro, then resurrect the unit that was reclaimed
-    if EntityCategoryContains(categories.NECRO, self) then
+    # If target is nil, then the wreckage has already been resurrected. This
+    # covers the case in which multiple necros are reclaiming the same wreckage.
+    if EntityCategoryContains(categories.NECRO, self) and target ~= nil and target.HasBeenResurrected ~= true then
       local blueprint = self.ReclaimTargetBP
       local army=self:GetArmy()
       local position=self.ReclaimTargetPosition
       CreateUnitHPR(blueprint, army, position[1], position[2], position[3], 0, 0, 0)
+      target.HasBeenResurrected = true
     end
 end,
 }
